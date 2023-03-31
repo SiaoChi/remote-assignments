e05282b41e94a3b891b430697a2b4e60e03df30c
KellyGuo <chichigou@gmail.com>
2023-03-31 15:10:44 +0800

    調整手機版header

---
+++ week-1/Assignment-3/count.js
@@ -0,0 +1,31 @@
+function countAandB(input) { // your code here
+    let ans = 0;
+    for(let i = 0 ; i < input.length ; i++){
+        if (input[i] === 'a' || input[i] === 'b'){
+            ans +=1;
+        }
+    }
+    return ans
+}
+function toNumber(input) { // your code here
+    let letter_val = {'a':1 , 'b':2 ,'c':3,'d':4,'e':5 };
+    let ans = [];
+    for (let i = 0 ; i < input.length ; i++){
+        ans.push(letter_val[input[i]])
+    };
+    return ans
+}
+
+let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
+console.log(countAandB(input1));
+// should print 4 (3 ‘a’ letters and 1 ‘b’ letter)
+
+console.log(toNumber(input1));
+// should print [1, 2, 3, 1, 3, 1, 3]
+
+let input2 = ['e', 'd', 'c', 'd', 'e'];
+console.log(countAandB(input2));
+// should print 0
+
+console.log(toNumber(input2));
+// should print [5, 4, 3, 4, 5]
