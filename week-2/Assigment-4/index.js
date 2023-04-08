5cfc4d31823d79056e9bd0d098151fd64c5f36c9
KellyGuo <chichigou@gmail.com>
2023-04-08 15:43:58 +0800

    debug -- btn要按第二次才會出現新的區塊 -- 修改index.js

--- week-2/Assigment-4/index.js
+++ week-2/Assigment-4/index.js
@@ -1,5 +1,4 @@
 
-
 const banner = document.querySelector(".banner")
 const banner_txt = document.querySelector("#banner-txt")
 banner.addEventListener('click',()=>{
@@ -11,9 +10,13 @@ const button = document.querySelector("#btn")
 const hideArea = document.querySelector(".content-box-container-2")
 
 button.addEventListener('click',()=> {
-    if(hideArea.style.display === 'none'){
+    console.log('點擊btn')
+    // "none"需要用 === ""空集合代表
+    if(hideArea.style.display == ""){
+        console.log('display: flex')
         hideArea.style.display = "flex"}
     else{
-        hideArea.style.display = "none"
+        console.log('display: none')
+        hideArea.style.display = ""
     }
 })
