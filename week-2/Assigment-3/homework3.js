06983d641b82c605d5fe6e0b3f8cfbf346fc18a1
KellyGuo <chichigou@gmail.com>
2023-04-08 15:41:46 +0800

    補上groupByKey function練習

--- week-2/Assigment-3/homework3.js
+++ week-2/Assigment-3/homework3.js
@@ -22,3 +22,25 @@ let input1 = ["a", "b", "c", "a", "c", "a", "x"];
 console.log(count(input1));
 // should print {a:3, b:1, c:2, x:1}
 
+function groupByKey(input) {
+    ans = {};
+    for (let i = 0 ; i < input.length ; i++){
+        if(ans[input[i].key]){
+            ans[input[i].key] = ans[input[i].key] + input[i].value
+        }
+        else{
+            ans[input[i].key] = input[i].value
+        }
+    }
+    return ans
+}
+
+let input2 = [
+{ key: "a", value: 3 },
+{ key: "b", value: 1 },
+{ key: "c", value: 2 },
+{ key: "a", value: 3 },
+{ key: "c", value: 5 },
+];
+console.log(groupByKey(input2)); // should print {a:6, b:1, c:7}
+
