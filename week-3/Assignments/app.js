1ff8e4b5a951ac8d274ef67cd9677faad2320fe3
KellyGuo <chichigou@gmail.com>
2023-04-17 09:56:51 +0800

    無法redirect成功--上傳分支給導師看

--- week-3/Assignments/app.js
+++ week-3/Assignments/app.js
@@ -8,7 +8,6 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.static('./public'))
 
-
 app.listen(3000, () => {
   console.log('Server is listening on port 3000....')
 })
@@ -38,39 +37,34 @@ app.get('/data',(req,res)=>{
 })
 
 
-app.get('/:myName',(req,res)=>{
-    console.log('----app.get----')
-    // console.log('req params: ' + req.params) // { myName: 'kelly' }
-    // console.log('Cookies: ', req.cookies)
-    // const username = req.cookies.username; //Cookies:  { csrftoken: 'lXg9QcohSul2QdbUeezTeeo2TOBTXRxS' }
-    const { username } = req.cookies
-    const { myName } = req.params; //取出url的:myName
-    if( username && username === myName){
-        res.send(`<h1>你的名字是${myName}</h1>`)
-    }else{
-        res.sendFile(path.resolve(__dirname,'./public/username.html'))
-    }
-})
+app.get('/myname',(req,res)=>{
+    console.log('----收到 myname get----')
+    const { name } = req.cookies
+    console.log("----")
+    console.log(name)
 
-//建立cookie的api，會回傳status200，讓前端可以redirect url到 /:myNmae
-app.post('/trackName',(req,res)=>{
-    console.log('----app.post----')
-    const {name} = req.body
     if(name){
-        res.cookie('username', name)
-            .status(200)
-            .json({success: true , name : name, redirected: true})
-            // .redirect(`/${name}`);
-            // 問題：可否在cookie建立後直接redirect到 /:myName，通常是前端還後端負責redirect?
-            /* ChatGTP :後端的 res.redirect() 是在伺服器端進行跳轉，因此前端畫面不會改變。
-            當瀏覽器收到來自伺服器端的重定向訊息後，它會自動發出新的請求，然後瀏覽器會載入新的頁面。*/
-
-    } else{
-        res.status(400).send('請重新輸入名字')
+        console.log(`你名字是${name}`)
+        // res.redirect("/")
+        const html = `<html><h1>你的名字是 ${name}</h1></html>`;
+        return res.send(html);
+        // res.sendFile(path.resolve(__dirname, './public/myname.html'), { name: name });
+    }else{
+        console.log('send insert name html ')
+        res.sendFile(path.resolve(__dirname,'./public/trackname.html'))
     }
 })
 
-
+app.get('/trackName',(req,res)=>{
+    console.log('----收到 trackname get----')
+    const { name } = req.query
+    if(name){
+         res.cookie('name', name)
+    } else{
+        res.sendFile(path.resolve(__dirname,'./public/trackname.html'))
+    }
+    return res.redirect(307,'/myname')
+})
 
 app.all('*', (req, res) => {
   res.status(404).send('<h1>resource not found</h1>')
